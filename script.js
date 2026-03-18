d91e9f8126888d6d6cbe44892380a183c94f825c
sergio-pj <sergiopaulo.almeida04@gmail.com>
2026-03-18 10:02:46 -0300

    feat: estrutura inicial da home, quem somos e destaques

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleMenu() {
         sidebar.style.width = "250px";
     }
 }
+
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    const overlay = document.getElementById("overlay");
+    
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        overlay.style.display = "none";
+    } else {
+        sidebar.style.width = "250px";
+        overlay.style.display = "block";
+    }
+}
